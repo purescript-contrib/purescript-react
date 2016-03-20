69cf38fe5805a745afe8950d416e07c4d61a55b3
Jason M. Zoladz <jasonzoladz@gmail.com>
2016-03-20 08:51:54 -0400

    Removed unused import

--- src/React/AddOns/Animation/TransitionFFI.js
+++ src/React/AddOns/Animation/TransitionFFI.js
@@ -2,7 +2,7 @@
 "use strict";
 
 // module React.Addons.Animation.Transition
-var React = require('react');
+
 var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
 
 exports.reactCSSTransitionGroupClass = ReactCSSTransitionGroup;
